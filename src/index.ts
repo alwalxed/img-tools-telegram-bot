58fff94782eac228c4d096c8b7266a377410f7db
systematicRealm <119763173+systematicRealm@users.noreply.github.com>
2024-07-02 10:14:54 +0300

    👌 IMPROVE: Removed retry func

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { Hono } from "hono";
 import TelegramBot from "node-telegram-bot-api";
 import { TOKEN, PORT } from "./constants";
 import { logger } from "./functions/logger";
-import { retryWithExponentialBackoff } from "./functions/retry";
 import { onMessageHandler } from "./functions/handlers";
 
 const app = new Hono();
@@ -13,12 +12,6 @@ bot.on("message", (msg) => onMessageHandler(bot, msg));
 
 bot.on("polling_error", async (error) => {
   logger("error", "polling_error", undefined, { error });
-  try {
-    await retryWithExponentialBackoff(bot);
-    logger("info", "polling_error", undefined, { error });
-  } catch (err) {
-    logger("error", "polling_error", undefined, { error: err });
-  }
 });
 
 bot.on("webhook_error", (error) => {
